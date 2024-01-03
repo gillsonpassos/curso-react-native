d16b05fa04eedfe6459c8f4b746ccd3241982a46
gillsonpassos <oliveirapassos07@outlook.com>
2024-01-03 10:09:54 -0300

    Organizando componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react"
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 
-function App() {
-   return <Text>Primeiro Componente 2</Text>
-}
+import './components/Primeiro'
+import Primeiro from "./components/Primeiro"
 
-export default App
+export default () => (
+    <View>
+        <Primeiro />
+    </View>
+)
